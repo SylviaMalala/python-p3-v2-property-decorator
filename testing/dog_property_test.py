146ee47ee2b7e76dbd7692d47b69ca2a9b556a64
SylviaMalala <sylviamalala17@gmail.com>
2025-11-26 16:44:33 +0300

    property

--- testing/dog_property_test.py
+++ testing/dog_property_test.py
@@ -8,24 +8,22 @@ class TestDogProperties:
     def test_name_breed_valid(self):
         '''validates name and breed properties are initialized with valid values'''
         try:
-            # No exception should be thrown since name and breed are valid
             dog = Dog("Fido", "Corgi")
         except Exception as exc:
-            # The assertion fails if an exception is thrown
             assert False, f'Dog("Fido", "Corgi") raised an exception {exc}'
 
     def test_name_is_string_valid_length(self):
         '''validates name property is assigned a string between 1 and 25 characters'''
         dog = Dog("Fido", "Corgi")
         with pytest.raises(ValueError):
-            dog.name = 7  # not a string
+            dog.name = 7
         with pytest.raises(ValueError):
-            dog.name = ''  # too short
+            dog.name = ''
         with pytest.raises(ValueError):
-            dog.name = 'Fido the adorable Corgi who likes to steal socks'  # too long
+            dog.name = 'Fido the adorable Corgi who likes to steal socks'
 
     def test_breed_is_approved_breed(self):
         '''validates breed property is in list of approved choices'''
         dog = Dog("Snoopy", "Beagle")
         with pytest.raises(ValueError):
-            dog.breed = "Poodle"  # not an approved breed
+            dog.breed = "Poodle"
